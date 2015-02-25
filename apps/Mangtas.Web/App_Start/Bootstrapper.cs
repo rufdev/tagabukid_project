d61c08c4730a63cb1d2e4306480dd5229be05efd
Rufino John Aguilar <aguilarufino@gmail.com>
2015-02-25 15:24:45 +0800

    delete test service

--- apps/Mangtas.Web/App_Start/Bootstrapper.cs
+++ apps/Mangtas.Web/App_Start/Bootstrapper.cs
@@ -27,38 +27,13 @@ namespace Mangtas.Web
         {
             var builder = new ContainerBuilder();
             builder.RegisterControllers(Assembly.GetExecutingAssembly());
-            builder.RegisterType<PersonContext>().As<IDataContextAsync>().InstancePerRequest();
+
+            //builder.RegisterType<PersonContext>().As<IDataContextAsync>().InstancePerRequest();
             builder.RegisterType<UnitOfWork>().As<IUnitOfWorkAsync>().InstancePerRequest();
 
-            builder.RegisterType<Repository<Person>>().As<IRepositoryAsync<Person>>().InstancePerRequest();
-            builder.RegisterType<PersonService>().As<IPersonService>().InstancePerRequest();
-
-            //builder.Register(_ => new PersonContext())
-            //       .As<IDataContextAsync>().InstancePerRequest();
-            //builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
-            //builder.RegisterType<Repository<Person>>().As<IRepositoryAsync<Person>>().InstancePerRequest();
-
-            //builder.RegisterControllers(typeof(MvcApplication).Assembly);
-            //builder.RegisterType<PersonContext>().As<IDataContextAsync>().InstancePerRequest();
-            //builder.RegisterType<UnitOfWork>().As<IUnitOfWork>().InstancePerRequest();
-            //builder.Register<PersonContext>((_) => new PersonContext()).InstancePerRequest();
             //builder.RegisterType<Repository<Person>>().As<IRepositoryAsync<Person>>().InstancePerRequest();
             //builder.RegisterType<PersonService>().As<IPersonService>().InstancePerRequest();
-            //   builder.RegisterAssemblyTypes(typeof(FocusRepository).Assembly)
-            //   .Where(t => t.Name.EndsWith("Repository"))
-            //   .AsImplementedInterfaces().InstancePerHttpRequest();
-            //   builder.RegisterAssemblyTypes(typeof(GoalService).Assembly)
-            //  .Where(t => t.Name.EndsWith("Service"))
-            //  .AsImplementedInterfaces().InstancePerHttpRequest();
 
-            //   builder.RegisterAssemblyTypes(typeof(DefaultFormsAuthentication).Assembly)
-            //.Where(t => t.Name.EndsWith("Authentication"))
-            //.AsImplementedInterfaces().InstancePerHttpRequest();
-
-            //   builder.Register(c => new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new SocialGoalEntities())))
-            //       .As<UserManager<ApplicationUser>>().InstancePerHttpRequest();
-
-            //builder.RegisterFilterProvider();
             var container = builder.Build();
             DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
